f2e27b3c6332976b221b4035104a5be757734bbb
solefreshofficial <solefreshofficial@gmail.com>
2026-01-11 15:37:20 +0530

    home back to home

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -232,7 +232,7 @@ const Shop = () => {
 
         {/* Back link */}
         <motion.a
-          href="/"
+          href="/Solefreshshoe-Deodorizer/"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.6 }}
